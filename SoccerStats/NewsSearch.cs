94e290b0ff763b0aed4b9b8f9ba1260a9f8d64e0
amills80 <aelonrench@yahoo.com>
2017-12-16 08:29:44 -0500

    Add Bing Text / Sentiment Analysis API to IO

--- SoccerStats/NewsSearch.cs
+++ SoccerStats/NewsSearch.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,91 +9,43 @@ namespace SoccerStats
     public class NewsSearch
     {
         public string _type { get; set; }
-        public Querycontext queryContext { get; set; }
-        public Webpages webPages { get; set; }
-        public Relatedsearches relatedSearches { get; set; }
-        public Rankingresponse rankingResponse { get; set; }
-    }
-
-    public class Querycontext
-    {
-        public string originalQuery { get; set; }
-    }
-
-    public class Webpages
-    {
-        public string webSearchUrl { get; set; }
+        public string readLink { get; set; }
         public int totalEstimatedMatches { get; set; }
-        public Value[] value { get; set; }
+        [JsonProperty(PropertyName = "value")]
+        public List<NewsResult> NewsResults { get; set; }
     }
 
-    public class Value
+    public class NewsResult
     {
-        public string id { get; set; }
-        public string name { get; set; }
+        [JsonProperty(PropertyName = "name")]
+        public string Headline { get; set; }
         public string url { get; set; }
-        public string displayUrl { get; set; }
-        public string snippet { get; set; }
-        public Deeplink[] deepLinks { get; set; }
+        public Image image { get; set; }
+        [JsonProperty(PropertyName = "description")]
+        public string Summary { get; set; }
+        public Provider[] provider { get; set; }
+        [JsonProperty(PropertyName = "datePublished")]
+        public DateTime DatePublished { get; set; }
+        public string category { get; set; }
+        public double SentimentScore { get; set; }
     }
 
-    public class Deeplink
+    public class Image
     {
+        public Thumbnail thumbnail { get; set; }
+    }
+
+    public class Thumbnail
+    {
+        public string contentUrl { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+    }
+
+    public class Provider
+    {
+        public string _type { get; set; }
         public string name { get; set; }
-        public string url { get; set; }
-        public string snippet { get; set; }
-    }
-
-    public class Relatedsearches
-    {
-        public string id { get; set; }
-        public Value1[] value { get; set; }
-    }
-
-    public class Value1
-    {
-        public string text { get; set; }
-        public string displayText { get; set; }
-        public string webSearchUrl { get; set; }
-    }
-
-    public class Rankingresponse
-    {
-        public Mainline mainline { get; set; }
-        public Sidebar sidebar { get; set; }
-    }
-
-    public class Mainline
-    {
-        public Item[] items { get; set; }
-    }
-
-    public class Item
-    {
-        public string answerType { get; set; }
-        public int resultIndex { get; set; }
-        public Value2 value { get; set; }
-    }
-
-    public class Value2
-    {
-        public string id { get; set; }
-    }
-
-    public class Sidebar
-    {
-        public Item1[] items { get; set; }
-    }
-
-    public class Item1
-    {
-        public string answerType { get; set; }
-        public Value3 value { get; set; }
-    }
-
-    public class Value3
-    {
-        public string id { get; set; }
     }
 
 }
--- SoccerStats/Program.cs
+++ SoccerStats/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using System.Text;
 using Newtonsoft.Json;
+//using NewsSearch;
 
 namespace SoccerStats
 {
@@ -10,21 +12,42 @@ namespace SoccerStats
     {
         static void Main(string[] args)
         {
-            //string currentDirectory = Directory.GetCurrentDirectory();
-            //DirectoryInfo directory = new DirectoryInfo(currentDirectory);
-            //var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
-            //var fileContents = ReadSoccerResults(fileName);
-            //fileName = Path.Combine(directory.FullName, "players.json");
-            //var players = DeserializePlayers(fileName);
-            //var topTenPlayers = GetTopTenPlayers(players);
-            //foreach(var player in topTenPlayers)
-            //{
-            //    Console.WriteLine("Name: " + player.FirstName + " PPG: " + player.PointsPerGame);
-            //}
-            //fileName = Path.Combine(directory.FullName, "topten.json");
-            //SerializePlayersToFile(topTenPlayers, fileName);
+            string currentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+            var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
+            var fileContents = ReadSoccerResults(fileName);
+            fileName = Path.Combine(directory.FullName, "players.json");
+            var players = DeserializePlayers(fileName);
+            var topTenPlayers = GetTopTenPlayers(players);
+            foreach (var player in topTenPlayers)
+            {
+                List<NewsResult> newsResults = GetNewsForPlayer(string.Format("{0} {1}", player.FirstName, player.SecondName));
+                SentimentResponse sentimentResponse = GetSentimentResponse(newsResults);
+                foreach(var sentiment in sentimentResponse.Sentiments)
+                {
+                    foreach (var newsResult in newsResults)
+                    {
+                        if (newsResult.Headline == sentiment.Id)
+                        {
+                            double score;
+                            if (double.TryParse(sentiment.Score, out score))
+                            {
+                                newsResult.SentimentScore = score;
+                            }
+                            break;
+                        }
+                    }
+                }
 
-            Console.WriteLine(GetNewsForPlayer("Diego Valeri"));
+                foreach (var result in newsResults)
+                {
+                    Console.WriteLine(string.Format("Sentiment Score: {0:P}, Date: {1:f} \r\n Headline: {2} \r\n Summary: {3} \r\n", result.SentimentScore, result.DatePublished, result.Headline, result.Summary));
+                    Console.ReadKey();
+                }
+            }
+            fileName = Path.Combine(directory.FullName, "topten.json");
+            SerializePlayersToFile(topTenPlayers, fileName);
+
         }
 
         public static string ReadFile(string fileName)
@@ -123,6 +146,7 @@ namespace SoccerStats
             return topTenPlayers;
 
         }
+
         public static void SerializePlayersToFile(List<Player> players, string fileName)
         {
             var serializer = new JsonSerializer();
@@ -146,17 +170,41 @@ namespace SoccerStats
             }
         }
 
-        public static string GetNewsForPlayer(string playerName)
+        public static List<NewsResult> GetNewsForPlayer(string playerName)
         {
+            var results = new List<NewsResult>();
             var webClient = new WebClient();
-            webClient.Headers.Add("Ocp-Apim-Subscription-Key", "52d7666180094721b62920132bb5bfb6");
+            webClient.Headers.Add("Ocp-Apim-Subscription-Key", "0604b891b2c2482d8e43d1be95842d55");
             byte[] searchResults = webClient.DownloadData(string.Format("https://api.cognitive.microsoft.com/bing/v7.0/news/search?q={0}&mkt=en-us", playerName));
-
+            var serializer = new JsonSerializer();
             using (var stream = new MemoryStream(searchResults))
             using (var reader = new StreamReader(stream))
+            using (var jsonReader = new JsonTextReader(reader))
             {
-                return reader.ReadToEnd();
+                results = serializer.Deserialize<NewsSearch>(jsonReader).NewsResults;
             }
+            return results;
+        }
+
+        public static SentimentResponse GetSentimentResponse(List<NewsResult> newsResults)
+        {
+            var sentimentResponse = new SentimentResponse();
+            var sentimentRequest = new SentimentRequest();
+            sentimentRequest.Documents = new List<Document>();
+            foreach(var result in newsResults)
+            {
+                sentimentRequest.Documents.Add(new Document { Id = result.Headline, Text = result.Summary });
+            }
+            var webClient = new WebClient();
+            webClient.Headers.Add("Ocp-Apim-Subscription-Key", "e5d20a8be53e40a195b344188f3634cb");
+            webClient.Headers.Add(HttpRequestHeader.Accept, "application/json");
+            webClient.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+            string requestJson = JsonConvert.SerializeObject(sentimentRequest);
+            byte[] requestBytes = Encoding.UTF8.GetBytes(requestJson);
+            byte[] response = webClient.UploadData("https://westcentralus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment", requestBytes);
+            string sentiments = Encoding.UTF8.GetString(response);
+            sentimentResponse = JsonConvert.DeserializeObject<SentimentResponse>(sentiments);
+            return sentimentResponse;
         }
     }
 }
